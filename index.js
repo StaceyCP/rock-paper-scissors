b20a553cb0bf7aef18c74a209889252528299588
Stacey <stacey6105@gmail.com>
2022-12-03 12:45:12 +0000

    Update computer choice img being shown this now works as expected

--- index.js
+++ index.js
@@ -2,10 +2,29 @@ let computerWinCount = 0;
 let playerWinCount = 0;
 let computersGo = Math.floor(Math.random() * 3);
 
-document.querySelector(".computer-attempt-image").className = `computer-attempt-image ${computersGo}`
+const computerChoices = document.getElementsByClassName("computer-attempt-image");
+for (let i = 0; i < computerChoices.length; i++) {
+    computerChoices[i].style.display = "none";
+}
+
+function changePcImg() {
+    if (computersGo === 0) {
+        document.querySelector(".rock").style.display = "block"
+        document.querySelector(".filler").style.display = "none"
+    } else if (computersGo === 1) {
+        document.querySelector(".paper").style.display = "block"
+        document.querySelector(".filler").style.display = "none"
+    } else if (computersGo === 2) {
+        document.querySelector(".scissors").style.display = "block"
+        document.querySelector(".filler").style.display = "none"
+    }
+}
+setTimeout(changePcImg, 2000)
+// document.querySelector(".computer-attempt-image").className = `computer-attempt-image ${computersGo}`
+// computerChoice.style.display = "block"
+
 document.querySelector(".player-score").innerHTML = playerWinCount;
 document.querySelector(".computer-score").innerHTML = computerWinCount;
 let gameStatus = document.querySelector(".game-status");
-
 
 console.log(computersGo);
