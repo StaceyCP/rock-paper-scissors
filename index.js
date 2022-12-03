f4b293e9e04f63c3b887f5811438044362ba9625
Stacey <stacey6105@gmail.com>
2022-12-03 00:21:58 +0000

    Update js file to change scores from count vars

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+// rps === rock paper scissors
+const rpsLookup = {
+    rock: 0,
+    paper: 1,
+    scissors: 2
+}
+
+
 let computerWinCount = 0;
 let playerWinCount = 0;
+let computersGo = Math.floor(Math.random() * 3);
+
+document.querySelector(".player-score").innerHTML = playerWinCount;
+document.querySelector(".computer-score").innerHTML = computerWinCount;
+let gameStatus = document.querySelector(".game-status");
+
+
+console.log(computersGo);
